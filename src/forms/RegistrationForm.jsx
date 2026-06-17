08f76dc804be3e74efbeb370226f2c556d0377fd
amdadul haque <amdadulhaque601671@gmail.com>
2026-06-17 14:11:27 +0600

    register form

--- src/forms/RegistrationForm.jsx
+++ src/forms/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { useFieldArray, useForm } from 'react-hook-form';
 import Field from '../components/Field';
 import FieldSet from '../components/FieldSet';
 
@@ -7,8 +7,13 @@ const RegistrationForm = () => {
         register,
         handleSubmit,
         formState: { errors },
-        setError,
+        control,
     } = useForm();
+
+    const { fields, append, remove } = useFieldArray({
+        name: 'socials',
+        control,
+    });
 
     const submitForm = (formData) => {
         console.log(formData);
@@ -32,13 +37,14 @@ const RegistrationForm = () => {
                     </Field>
 
                     <Field label={'Your id'} error={errors.id}>
-                        <input {...register('id', {
-                            required: 'ID is required',
-                            max: {
-                                value: 1000,
-                                message:"Your id must be 1 to 1000"
-                            }
-                        })}
+                        <input
+                            {...register('id', {
+                                required: 'ID is required',
+                                max: {
+                                    value: 1000,
+                                    message: 'Your id must be 1 to 1000',
+                                },
+                            })}
                             className="p-2 border border-box rounded-md w-75 border-gray-200"
                             type="number"
                             placeholder="Enter your id"
@@ -77,8 +83,51 @@ const RegistrationForm = () => {
                         />
                     </Field>
                 </FieldSet>
+
+                <FieldSet label={'Enter social Handles'}>
+                    {fields.map((field, index) => {
+                        return (
+                            <div
+                                key={field.id}
+                                className="flex justify-between items-center w-max "
+                            >
+                                <Field label={'Social Name'}>
+                                    <input
+                                        {...register(`socials[${index}].name`)}
+                                        className="p-2 border box-border w-full rounded-md"
+                                        type="text"
+                                        name=""
+                                        id={`socials[${index}].name`}
+                                    />
+                                </Field>
+
+                                <Field label={'Social URL'}>
+                                    <input
+                                        {...register(`socials[${index}].url`)}
+                                        className="p-2 border box-border w-full rounded-md"
+                                        type="text"
+                                        name={`socials[${index}].url`}
+                                        id={`socials[${index}].url`}
+                                    />
+                                </Field>
+                                <button
+                                    onClick={() => remove(index)}
+                                    className="mt-8 mr-2 text-2xl cursor-pointer"
+                                >
+                                    &#8722;
+                                </button>
+                            </div>
+                        );
+                    })}
+                    <button
+                        className="p-2 mt-2 border border-box rounded-lg  bg-gray-500 text-white cursor-pointer"
+                        onClick={() => append({ name: '', url: '' })}
+                    >
+                        Add a social handle
+                    </button>
+                </FieldSet>
                 <Field>
-                    <button className="text-md text-white px-2 py-1 border rounded-lg cursor-pointer bg-purple-500 m-2">
+                    <button className="text-md text-white px-2 py-1 border rounded-lg cursor-pointer bg-purple-500 m-auto">
                         Register
                     </button>
                 </Field>
